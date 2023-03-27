1b5d46a696469ac9b245de6b34c69071c1cabbf5
Hannah O'Malley <halcyonshift@gmail.com>
2023-03-27 16:48:07 +0100

    Scrolling tabs

--- src/StoryLine/layouts/Work/Tabs/index.tsx
+++ src/StoryLine/layouts/Work/Tabs/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
+import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import { Tabs as MuiTabs } from '@mui/material'
 import Tab from '@mui/material/Tab'
@@ -8,10 +9,12 @@ import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautif
 import { useTranslation } from 'react-i18next'
 import { getHex } from '@sl/theme/utils'
 import useTabs from './useTabs'
+import useLayout from '../useLayout'
 
 const Tabs = () => {
     const { t } = useTranslation()
     const tabs = useTabs()
+    const { windowWidth, panelWidth, navigationWidth } = useLayout()
 
     const onDragEnd = (result: DropResult) => {
         if (!result.destination) return
@@ -27,77 +30,84 @@ const Tabs = () => {
     return useMemo(
         () =>
             tabs.showTabs ? (
-                <DragDropContext onDragEnd={onDragEnd}>
-                    <Droppable droppableId='tabs'>
-                        {(props) => (
-                            <MuiTabs
-                                ref={props.innerRef}
-                                {...props.droppableProps}
-                                value={tabs.active}
-                                onChange={(_, value) => tabs.setActive(value)}
-                                variant='scrollable'
-                                scrollButtons={false}
-                                aria-label={t('layout.work.tabs')}
-                                TabIndicatorProps={{
-                                    style: {
-                                        display: 'none',
-                                        backgroundColor: getHex('slate', 100)
-                                    }
-                                }}>
-                                {tabs.tabs.map((tab, index) => (
-                                    <Draggable
-                                        key={tab.id}
-                                        draggableId={`id-${tab.id}`}
-                                        index={index}
-                                        disableInteractiveElementBlocking={true}>
-                                        {(props) => (
-                                            <Tab
-                                                sx={{
-                                                    marginLeft: '1px',
-                                                    opacity: 1,
-                                                    backgroundColor:
-                                                        index === tabs.active
-                                                            ? getHex('slate', 100)
-                                                            : 'white'
-                                                }}
-                                                ref={props.innerRef}
-                                                {...props.draggableProps}
-                                                onClick={(e) => {
-                                                    e.stopPropagation()
-                                                    tabs.setActive(index)
-                                                }}
-                                                iconPosition='start'
-                                                wrapped
-                                                label={
-                                                    <Stack
-                                                        direction='row'
-                                                        className='justify-middle'
-                                                        spacing={2}>
-                                                        <Typography
-                                                            variant='body2'
-                                                            className='max-w-[150px] truncate'
-                                                            {...props.dragHandleProps}>
-                                                            {tab.label}
-                                                        </Typography>
-                                                        <CloseIcon
-                                                            fontSize='small'
-                                                            color='action'
-                                                            onClick={(e) => {
-                                                                e.stopPropagation()
-                                                                tabs.removeTab(tab.id)
-                                                            }}
-                                                        />
-                                                    </Stack>
-                                                }
-                                            />
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {props.placeholder}
-                            </MuiTabs>
-                        )}
-                    </Droppable>
-                </DragDropContext>
+                <Box
+                    sx={{
+                        marginLeft: '1px',
+                        maxWidth: windowWidth - (navigationWidth + panelWidth),
+                        bgcolor: 'background.paper'
+                    }}>
+                    <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable droppableId='tabs'>
+                            {(props) => (
+                                <MuiTabs
+                                    ref={props.innerRef}
+                                    {...props.droppableProps}
+                                    value={tabs.active}
+                                    onChange={(_, value) => tabs.setActive(value)}
+                                    variant='scrollable'
+                                    scrollButtons={false}
+                                    aria-label={t('layout.work.tabs')}
+                                    TabIndicatorProps={{
+                                        style: {
+                                            display: 'none',
+                                            backgroundColor: getHex('slate', 100)
+                                        }
+                                    }}>
+                                    {tabs.tabs.map((tab, index) => (
+                                        <Draggable
+                                            key={tab.id}
+                                            draggableId={`id-${tab.id}`}
+                                            index={index}
+                                            disableInteractiveElementBlocking={true}>
+                                            {(props) => (
+                                                <Tab
+                                                    sx={{
+                                                        marginLeft: '1px',
+                                                        opacity: 1,
+                                                        backgroundColor:
+                                                            index === tabs.active
+                                                                ? getHex('slate', 100)
+                                                                : 'white'
+                                                    }}
+                                                    ref={props.innerRef}
+                                                    {...props.draggableProps}
+                                                    onClick={(e) => {
+                                                        e.stopPropagation()
+                                                        tabs.setActive(index)
+                                                    }}
+                                                    iconPosition='start'
+                                                    wrapped
+                                                    label={
+                                                        <Stack
+                                                            direction='row'
+                                                            className='justify-middle'
+                                                            spacing={2}>
+                                                            <Typography
+                                                                variant='body2'
+                                                                className='max-w-[150px] truncate'
+                                                                {...props.dragHandleProps}>
+                                                                {tab.label}
+                                                            </Typography>
+                                                            <CloseIcon
+                                                                fontSize='small'
+                                                                color='action'
+                                                                onClick={(e) => {
+                                                                    e.stopPropagation()
+                                                                    tabs.removeTab(tab.id)
+                                                                }}
+                                                            />
+                                                        </Stack>
+                                                    }
+                                                />
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {props.placeholder}
+                                </MuiTabs>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
+                </Box>
             ) : null,
         [tabs.tabs, tabs.active, tabs.showTabs]
     )
