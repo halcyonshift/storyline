32c0e2992f11484635ff77b39bf033fe01989c29
Hannah O'Malley <halcyonshift@gmail.com>
2023-04-14 20:53:36 +0100

    Add licensing

--- src/StoryLine/views/storyline/Info/index.tsx
+++ src/StoryLine/views/storyline/Info/index.tsx
@@ -1,13 +1,47 @@
-import Box from '@mui/material/Box'
+import { Box, Stack } from '@mui/material'
 import Typography from '@mui/material/Typography'
+import { DateTime } from 'luxon'
 import { useTranslation } from 'react-i18next'
+import { htmlParse } from '@sl/utils'
 
 const InfoView = () => {
     const { t } = useTranslation()
 
     return (
-        <Box>
-            <Typography variant='h4'>{t('view.storyline.info.about.title')}</Typography>
+        <Box className='grid grid-cols-2 gap-3'>
+            <Box>
+                <Typography variant='h6'>{t('view.storyline.info.about.title')}</Typography>
+                <Stack spacing={1} className='mt-3'>
+                    <Box>
+                        <Typography variant='body2' className='text-slate-600'>
+                            {t('view.storyline.info.about.version')}
+                        </Typography>
+                        <Typography variant='body1'>{process.env.VERSION}</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant='body2' className='text-slate-600'>
+                            {t('view.storyline.info.about.developer')}
+                        </Typography>
+                        <Typography variant='body1'>Hannah O'Malley</Typography>
+                    </Box>
+                </Stack>
+            </Box>
+            <Box>
+                <Typography variant='h6'>{t('view.storyline.info.license.title')}</Typography>
+                <Typography variant='body1' className='pt-3'>
+                    {t('view.storyline.info.license.text')}
+                </Typography>
+                <Typography variant='body1'>
+                    {t('view.storyline.info.license.copyright', {
+                        symbol: htmlParse('&copy;'),
+                        year: DateTime.now().toFormat('yyyy')
+                    })}
+                </Typography>
+                <Typography variant='body2' className='pt-2 pb-1'>
+                    {t('view.storyline.info.license.notice1')}
+                </Typography>
+                <Typography variant='body2'>{t('view.storyline.info.license.notice2')}</Typography>
+            </Box>
         </Box>
     )
 }
