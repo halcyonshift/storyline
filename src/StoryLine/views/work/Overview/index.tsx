4fa23991cca51b3722b151be0a8c35180bb9efcb
Hannah O'Malley <halcyonshift@gmail.com>
2023-04-25 09:27:13 +0100

    Overview to tabs

--- src/StoryLine/views/work/Overview/index.tsx
+++ src/StoryLine/views/work/Overview/index.tsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from 'react'
-import { Box, Button, ButtonGroup, Divider } from '@mui/material'
+import { useEffect, useState, SyntheticEvent } from 'react'
+import { FilterList as FilterListIcon } from '@mui/icons-material'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
+import { Box, IconButton, Menu, MenuItem, Tab, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useRouteLoaderData } from 'react-router-dom'
 import { OverviewViewOption, OverviewViewOptionType } from '@sl/constants/overviewView'
@@ -8,12 +12,15 @@ import { SectionModel, WorkModel } from '@sl/db/models'
 import Summary from './Summary'
 import Timeline from './Timeline'
 
+const PADDING = { padding: 0 }
+
 const OverviewView = () => {
     const work = useRouteLoaderData('work') as WorkModel
+    const [value, setValue] = useState<OverviewViewOptionType>(OverviewViewOption.SUMMARY)
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
     const [chapters, setChapters] = useState<SectionModel[]>([])
     const [parts, setParts] = useState<SectionModel[]>([])
     const [scenes, setScenes] = useState<SectionModel[]>([])
-    const [view, setView] = useState<OverviewViewOptionType>(OverviewViewOption.SUMMARY)
     const { setShowTabs } = useTabs()
     const { t } = useTranslation()
 
@@ -25,27 +32,62 @@ const OverviewView = () => {
     }, [])
 
     return (
-        <Box className='flex flex-col flex-grow'>
-            <ButtonGroup disableElevation className='p-3'>
-                <Button
-                    variant={view === OverviewViewOption.SUMMARY ? 'contained' : 'outlined'}
-                    onClick={() => setView(OverviewViewOption.SUMMARY)}>
-                    {t('view.work.overview.summary.title')}
-                </Button>
-                <Button
-                    variant={view === OverviewViewOption.TIMELINE ? 'contained' : 'outlined'}
-                    onClick={() => setView(OverviewViewOption.TIMELINE)}>
-                    {t('view.work.overview.timeline.title')}
-                </Button>
-            </ButtonGroup>
-            <Divider />
-            <Box className='flex-grow h-0 overflow-auto p-3'>
-                {view === OverviewViewOption.SUMMARY ? (
+        <Box className=' w-full'>
+            <TabContext value={value}>
+                <Box className='border-b'>
+                    <TabList
+                        variant='fullWidth'
+                        onChange={(_: SyntheticEvent, newValue: string) => {
+                            setValue(newValue as OverviewViewOptionType)
+                        }}
+                        aria-label=''>
+                        <Tab
+                            label={t('view.work.overview.summary.title')}
+                            value={OverviewViewOption.SUMMARY}
+                        />
+                        <Tab
+                            label={
+                                <Box className='w-full flex justify-between'>
+                                    <Typography variant='body2'>
+                                        {t('view.work.overview.timeline.title')}
+                                    </Typography>
+                                    {value === OverviewViewOption.TIMELINE ? (
+                                        <IconButton
+                                            sx={{ padding: 0 }}
+                                            id='filter-button'
+                                            aria-controls={anchorEl ? 'filter-menu' : undefined}
+                                            aria-haspopup='true'
+                                            aria-expanded={anchorEl ? 'true' : undefined}
+                                            onClick={(e) => {
+                                                e.stopPropagation()
+                                                setAnchorEl(e.currentTarget)
+                                            }}>
+                                            <FilterListIcon fontSize='small' />
+                                        </IconButton>
+                                    ) : null}
+                                </Box>
+                            }
+                            value={OverviewViewOption.TIMELINE}
+                        />
+                    </TabList>
+                </Box>
+                <TabPanel value={OverviewViewOption.SUMMARY}>
                     <Summary parts={parts} chapters={chapters} scenes={scenes} />
-                ) : (
+                </TabPanel>
+                <TabPanel value={OverviewViewOption.TIMELINE} sx={PADDING}>
                     <Timeline work={work} />
-                )}
-            </Box>
+                </TabPanel>
+            </TabContext>
+            <Menu
+                id='filter-menu'
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+                MenuListProps={{
+                    'aria-labelledby': 'filter-button'
+                }}>
+                <MenuItem>Filter</MenuItem>
+            </Menu>
         </Box>
     )
 }
