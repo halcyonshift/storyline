b6aca403ea0c4e69dec498abe4f0a0bcbadb8706
Hannah O'Malley <halcyonshift@gmail.com>
2023-04-27 08:51:26 +0100

    Remove useMemo

--- src/StoryLine/views/work/Dashboard/Boxes/Tracker/PartList.tsx
+++ src/StoryLine/views/work/Dashboard/Boxes/Tracker/PartList.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useMemo } from 'react'
+import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Box, Typography } from '@mui/material'
 import Progress from '@sl/components/Progress'
@@ -10,44 +10,41 @@ import { PartListProps } from './types'
 const PartList = ({ parts, chapters, scenes }: PartListProps) => {
     const { t } = useTranslation()
 
-    return useMemo(
-        () => (
-            <>
-                {parts.map((part) => (
-                    <Fragment key={part.id}>
-                        <Box
-                            className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
-                            sx={{
-                                backgroundColor: status(part.status, 200).color,
-                                marginBottom: '1px'
-                            }}>
-                            <Box className='col-span-2 px-2 py-1'>
-                                <Typography
-                                    variant='body1'
-                                    className='whitespace-nowrap overflow-hidden text-ellipsis
-                            pr-3'>
-                                    {part.displayTitle}
+    return (
+        <>
+            {parts.map((part) => (
+                <Fragment key={part.id}>
+                    <Box
+                        className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
+                        sx={{
+                            backgroundColor: status(part.status, 200).color,
+                            marginBottom: '1px'
+                        }}>
+                        <Box className='col-span-2 px-2 py-1'>
+                            <Typography
+                                variant='body1'
+                                className='whitespace-nowrap overflow-hidden text-ellipsis
+                pr-3'>
+                                {part.displayTitle}
+                            </Typography>
+                            {part.daysRemaining ? (
+                                <Typography variant='body2'>
+                                    {t('view.work.dashboard.tracker.remaining', {
+                                        days: part.daysRemaining,
+                                        words: part.wordsPerDay
+                                    })}
                                 </Typography>
-                                {part.daysRemaining ? (
-                                    <Typography variant='body2'>
-                                        {t('view.work.dashboard.tracker.remaining', {
-                                            days: part.daysRemaining,
-                                            words: part.wordsPerDay
-                                        })}
-                                    </Typography>
-                                ) : null}
-                            </Box>
-                            <Progress words={part.wordCount} goal={part.wordGoal} />
+                            ) : null}
                         </Box>
-                        <ChapterList
-                            chapters={chapters.filter((chapter) => chapter.section.id === part.id)}
-                            scenes={scenes}
-                        />
-                    </Fragment>
-                ))}
-            </>
-        ),
-        []
+                        <Progress words={part.wordCount} goal={part.wordGoal} />
+                    </Box>
+                    <ChapterList
+                        chapters={chapters.filter((chapter) => chapter.section.id === part.id)}
+                        scenes={scenes}
+                    />
+                </Fragment>
+            ))}
+        </>
     )
 }
 
--- src/StoryLine/views/work/Dashboard/Boxes/Tracker/SceneList.tsx
+++ src/StoryLine/views/work/Dashboard/Boxes/Tracker/SceneList.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useMemo } from 'react'
+import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Box, Typography } from '@mui/material'
 import Progress from '@sl/components/Progress'
@@ -8,40 +8,37 @@ import { SceneListProps } from './types'
 const SceneList = ({ scenes }: SceneListProps) => {
     const { t } = useTranslation()
 
-    return useMemo(
-        () => (
-            <>
-                {scenes.map((scene) => (
-                    <Fragment key={scene.id}>
-                        <Box
-                            className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
-                            sx={{
-                                backgroundColor: status(scene.status).color,
-                                marginBottom: '1px'
-                            }}>
-                            <Box className='col-span-2 px-2 py-1'>
-                                <Typography
-                                    variant='body1'
-                                    className='whitespace-nowrap overflow-hidden text-ellipsis
-                            pr-3'>
-                                    {scene.displayTitle}
+    return (
+        <>
+            {scenes.map((scene) => (
+                <Fragment key={scene.id}>
+                    <Box
+                        className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
+                        sx={{
+                            backgroundColor: status(scene.status).color,
+                            marginBottom: '1px'
+                        }}>
+                        <Box className='col-span-2 px-2 py-1'>
+                            <Typography
+                                variant='body1'
+                                className='whitespace-nowrap overflow-hidden text-ellipsis
+                pr-3'>
+                                {scene.displayTitle}
+                            </Typography>
+                            {scene.daysRemaining ? (
+                                <Typography variant='body2'>
+                                    {t('view.work.dashboard.tracker.remaining', {
+                                        days: scene.daysRemaining,
+                                        words: scene.wordsPerDay
+                                    })}
                                 </Typography>
-                                {scene.daysRemaining ? (
-                                    <Typography variant='body2'>
-                                        {t('view.work.dashboard.tracker.remaining', {
-                                            days: scene.daysRemaining,
-                                            words: scene.wordsPerDay
-                                        })}
-                                    </Typography>
-                                ) : null}
-                            </Box>
-                            <Progress words={scene.wordCount} goal={scene.wordGoal} />
+                            ) : null}
                         </Box>
-                    </Fragment>
-                ))}
-            </>
-        ),
-        []
+                        <Progress words={scene.wordCount} goal={scene.wordGoal} />
+                    </Box>
+                </Fragment>
+            ))}
+        </>
     )
 }
 
