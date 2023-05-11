cd483feff35b6345e8997abd0b573be1c7d2a8ee
Hannah O'Malley <halcyonshift@gmail.com>
2023-05-11 12:02:41 +0100

    Updated sentry capture message

--- src/StoryLine/db/index.ts
+++ src/StoryLine/db/index.ts
@@ -21,30 +21,7 @@ const adapter = new LokiJSAdapter({
     // migrations,
     useWebWorker: false,
     useIncrementalIndexedDB: true,
-    dbName: process.env.DB_NAME,
-    onQuotaExceededError: () => {
-        // ToDo - Error catch
-        // Browser ran out of disk space -- offer the user to reload the app or log out
-    },
-    onSetUpError: () => {
-        // ToDo - Error catch
-        // Database failed to load -- offer the user to reload the app or log out
-    },
-    extraIncrementalIDBOptions: {
-        onDidOverwrite: () => {
-            // Called when this adapter is forced to overwrite contents of IndexedDB.
-            // This happens if there's another open tab of the same app that's making changes.
-            // Try to synchronize the app now, and if user is offline, alert them that if they
-            // close this tab, some data may be lost
-        },
-        onversionchange: () => {
-            // database was deleted in another browser tab (user logged out), so we must make sure
-            // we delete it in this tab as well - usually best to just refresh the page
-            // if (checkIfUserIsLoggedIn()) {
-            //  window.location.reload()
-            //}
-        }
-    }
+    dbName: process.env.DB_NAME
 })
 
 // Then, make a Watermelon database from it!
