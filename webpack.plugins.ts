f564dd72baa483a2ce7008feacf6e2ac3b130f71
Hannah O'Malley <halcyonshift@gmail.com>
2023-05-12 14:21:41 +0100

    Tidy config

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,7 @@
 import { WebpackPluginInstance } from 'webpack'
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import Dotenv from 'dotenv-webpack'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export const plugins = [
     new Dotenv({ path: `.env.${process.env.ENVIRONMENT}` }) as unknown as WebpackPluginInstance,
