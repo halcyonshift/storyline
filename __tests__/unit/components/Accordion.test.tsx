9906575b8b630435cbee94cf0962afce89f80b61
Hannah O'Malley <halcyonshift@gmail.com>
2023-05-14 21:05:57 +0100

    WIP

--- __tests__/unit/components/Accordion.test.tsx
+++ __tests__/unit/components/Accordion.test.tsx
@@ -4,20 +4,20 @@ import Accordion from '@sl/components/Accordion'
 import { render, screen } from '../test-utils'
 
 describe('<Accordion />', () => {
-    const accordion = (
-        <Accordion title={<Typography>Title</Typography>}>
-            <Typography>Show me</Typography>
-        </Accordion>
-    )
+    beforeEach(async () => {
+        render(
+            <Accordion title={<Typography>Title</Typography>}>
+                <Typography>Show me</Typography>
+            </Accordion>
+        )
+    })
 
     it('is closed by default', async () => {
-        render(accordion)
         expect(screen.getByText('Title')).toBeTruthy()
         expect(screen.queryByText('Show me')).toBeFalsy()
     })
 
     it('opens on title click', async () => {
-        render(accordion)
         await userEvent.click(screen.getByRole('button'))
         expect(screen.getByText('Show me')).toBeTruthy()
     })
