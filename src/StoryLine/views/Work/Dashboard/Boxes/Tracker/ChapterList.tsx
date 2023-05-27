1dd834cdcb40f95a8eeaa43ba6c822c9e28f3baa
Hannah O'Malley <halcyonshift@gmail.com>
2023-05-27 22:19:02 +0100

    Tracker redo

--- src/StoryLine/views/Work/Dashboard/Boxes/Tracker/ChapterList.tsx
+++ src/StoryLine/views/Work/Dashboard/Boxes/Tracker/ChapterList.tsx
@@ -1,60 +1,17 @@
 import { Fragment } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Box, Typography } from '@mui/material'
 import Progress from '@sl/components/Progress'
-import { status } from '@sl/theme/utils'
 import SceneList from './SceneList'
 import { ChapterListProps } from './types'
-import useSettings from '@sl/theme/useSettings'
 
-const ChapterList = ({ chapters, scenes }: ChapterListProps) => {
-    const { t } = useTranslation()
-    const settings = useSettings()
-
-    return (
-        <>
-            {chapters.map((chapter) => (
-                <Fragment key={chapter.id}>
-                    <Box
-                        className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
-                        sx={{
-                            backgroundColor: status(chapter.status, settings.isDark() ? 800 : 100)
-                                .color,
-                            marginBottom: '1px'
-                        }}>
-                        <Box
-                            className={`${
-                                chapter.wordGoal ? 'col-span-2' : 'col-span-3'
-                            } px-2 py-1`}>
-                            <Typography
-                                variant='body1'
-                                className='whitespace-nowrap overflow-hidden text-ellipsis
-                flex justify-between'>
-                                {chapter.displayTitle}
-                                {chapter.wordCount && !chapter.wordGoal ? (
-                                    <span>
-                                        {chapter.wordCount?.toLocaleString(settings.language)}
-                                    </span>
-                                ) : (
-                                    ''
-                                )}
-                            </Typography>
-                            {chapter.daysRemaining && chapter.wordGoal ? (
-                                <Typography variant='body2'>
-                                    {t('view.work.dashboard.tracker.remaining', {
-                                        days: chapter.daysRemaining,
-                                        words: chapter.wordsPerDay
-                                    })}
-                                </Typography>
-                            ) : null}
-                        </Box>
-                        <Progress words={chapter.wordCount} goal={chapter.wordGoal} />
-                    </Box>
-                    <SceneList scenes={scenes.filter((scene) => scene.section.id === chapter.id)} />
-                </Fragment>
-            ))}
-        </>
-    )
-}
+const ChapterList = ({ chapters, scenes }: ChapterListProps) => (
+    <>
+        {chapters.map((chapter) => (
+            <Fragment key={chapter.id}>
+                <Progress section={chapter} />
+                <SceneList scenes={scenes.filter((scene) => scene.section.id === chapter.id)} />
+            </Fragment>
+        ))}
+    </>
+)
 
 export default ChapterList
--- src/StoryLine/views/Work/Dashboard/Boxes/Tracker/PartList.tsx
+++ src/StoryLine/views/Work/Dashboard/Boxes/Tracker/PartList.tsx
@@ -1,52 +1,21 @@
 import { Fragment } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Box, Typography } from '@mui/material'
 import Progress from '@sl/components/Progress'
-import useSettings from '@sl/theme/useSettings'
-import { status } from '@sl/theme/utils'
 
 import ChapterList from './ChapterList'
 import { PartListProps } from './types'
 
-const PartList = ({ parts, chapters, scenes }: PartListProps) => {
-    const { t } = useTranslation()
-    const { isDark } = useSettings()
-
-    return (
-        <>
-            {parts.map((part) => (
-                <Fragment key={part.id}>
-                    <Box
-                        className='grid grid-cols-1 xl:grid-cols-3 gap-1 xl:pr-2'
-                        sx={{
-                            backgroundColor: status(part.status, isDark() ? 900 : 200).color,
-                            marginBottom: '1px'
-                        }}>
-                        <Box className='col-span-2 px-2 py-1'>
-                            <Typography
-                                variant='body1'
-                                className='whitespace-nowrap overflow-hidden text-ellipsis pr-3'>
-                                {part.displayTitle}
-                            </Typography>
-                            {part.daysRemaining ? (
-                                <Typography variant='body2'>
-                                    {t('view.work.dashboard.tracker.remaining', {
-                                        days: part.daysRemaining,
-                                        words: part.wordsPerDay
-                                    })}
-                                </Typography>
-                            ) : null}
-                        </Box>
-                        <Progress words={part.wordCount} goal={part.wordGoal} />
-                    </Box>
-                    <ChapterList
-                        chapters={chapters.filter((chapter) => chapter.section.id === part.id)}
-                        scenes={scenes}
-                    />
-                </Fragment>
-            ))}
-        </>
-    )
-}
+const PartList = ({ parts, chapters, scenes }: PartListProps) => (
+    <>
+        {parts.map((part) => (
+            <Fragment key={part.id}>
+                <Progress section={part} />
+                <ChapterList
+                    chapters={chapters.filter((chapter) => chapter.section.id === part.id)}
+                    scenes={scenes}
+                />
+            </Fragment>
+        ))}
+    </>
+)
 
 export default PartList
