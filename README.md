c522bf134fbbbe40892386b23e25cb0c030b452c
Hannah O'Malley <halcyonshift@gmail.com>
2023-06-09 08:50:54 +0100

    Update README and win builder

--- README.md
+++ README.md
@@ -57,8 +57,23 @@ $ sonar-scanner \
     -Dsonar.tests=__tests__/unit
 ```
 
-## SENTRY
+## Sentry
 
 ```shell
 $ yarn test:unit
 $ yarn test:integration
+```
+
+## Build
+
+```shell
+docker run --rm -ti \
+ --env-file="./.env.production" \
+ --env ELECTRON_CACHE="/root/.cache/electron" \
+ --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
+ -v ${PWD}:/project \
+ -v ${PWD##*/}-node-modules:/project/node_modules \
+ -v ~/.cache/electron:/root/.cache/electron \
+ -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+ electronuserland/builder:wine
+```
--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -4,9 +4,15 @@ import Dotenv from 'dotenv-webpack'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export const plugins = [
-    new Dotenv({
-        path: `./.env.${process.env.ENVIRONMENT === 'development' ? 'development' : 'production'}`
-    }) as unknown as WebpackPluginInstance,
+    new Dotenv(
+        process.env.CIRCLE && parseInt(process.env.CIRCLE) !== 1
+            ? {
+                  path: `./.env.${
+                      process.env.ENVIRONMENT === 'development' ? 'development' : 'production'
+                  }`
+              }
+            : {}
+    ) as unknown as WebpackPluginInstance,
     new ForkTsCheckerWebpackPlugin({
         logger: 'webpack-infrastructure'
     }),
